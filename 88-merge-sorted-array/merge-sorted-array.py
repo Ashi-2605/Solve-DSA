9d0c0be825a231547d16801cad6f3bb24a2d39dc
ASHI JAIN <ashirk2605@gmail.com>
2026-06-26 20:39:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (20.45%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        p1 = m - 1
+        p2 = n - 1
+        writePos = m + n - 1
+
+        while p2 >= 0:
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[writePos] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[writePos] = nums2[p2]
+                p2 -= 1
+
+            writePos -= 1
